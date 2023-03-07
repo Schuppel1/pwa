eed35c0e21da0903008782441187624f59e81035
Schuppel1 <sven.schu.mobil@gmail.com>
2023-03-07 15:13:05 +0100

    Add: login and settings for future use

--- my-minigolf/src/app/app.module.ts
+++ my-minigolf/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AboutComponent } from './about/about.component';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { StartComponent } from './start/start.component';
 import { ScorecardLocalComponent } from './scorecard-local/scorecard-local.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { LoginSelectComponent } from './login-select/login-select.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { ScorecardLocalComponent } from './scorecard-local/scorecard-local.compo
     AboutComponent,
     ImpressumComponent,
     StartComponent,
-    ScorecardLocalComponent
+    ScorecardLocalComponent,
+    LoginScreenComponent,
+    LoginSelectComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-minigolf/src/app/login-select/login-select.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-select',
+  templateUrl: './login-select.component.html',
+  styleUrls: ['./login-select.component.css']
+})
+export class LoginSelectComponent {
+
+}
