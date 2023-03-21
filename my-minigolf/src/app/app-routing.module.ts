ad3234b5c0f8b1a03049fa3dab401ab7e28f9b69
Schuppel1 <sven.schu.mobil@gmail.com>
2023-03-21 09:30:36 +0100

    Add Routing for Login/SignUp

--- my-minigolf/src/app/app-routing.module.ts
+++ my-minigolf/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SignupComponent } from './login-screen/signup.component';
+import { LoginScreenComponent } from "./login-screen/login-screen.component";
+import { StartComponent } from './start/start.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: '', component: StartComponent },
+  { path: 'login', component: LoginScreenComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'impressum', component: ImpressumComponent },
+  { path: 'about', component: AboutComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
